38effd5573faad5e43c50abee77dc1430e0aa250
imwaa <walid.dev@outlook.fr>
2021-01-13 19:23:07 +0100

    updated navbar

--- app/views/include/navbar.php
+++ app/views/include/navbar.php
@@ -1,19 +1,21 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
-    <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
-    <div class="justify-content-start" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="<?php echo URLROOT; ?>">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>pages/about">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>users/login">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>users/register">register</a>
-            </li>
-        </ul>
+    <div class="container">
+        <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
+        <div class="justify-content-start" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>pages/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>users/login">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>users/register">register</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
